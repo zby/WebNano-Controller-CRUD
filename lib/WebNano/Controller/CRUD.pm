21de1e1ba92ac846fcf47f33844d2dd63df9df27
Zbigniew Lukasiak <zby@cpan.org>
2010-10-18 11:34:46 +0200

    after_POST; packaging changes

--- lib/WebNano/Controller/CRUD.pm
+++ lib/WebNano/Controller/CRUD.pm
@@ -77,6 +77,11 @@ sub list_action {
     return $self->render( template => 'list.tt', items => [ $rs->search ] );
 }
 
+sub after_POST {
+    my( $self, @ids ) = @_;
+    return $self->self_url . join( '/', @ids ) . '/view';
+}
+
 sub create_action {
     my ( $self ) = @_;
     my $req = $self->req;
@@ -92,7 +97,7 @@ sub create_action {
     if( $req->method eq 'POST' && $form->process() ){
         my $record = $form->item;
         my $res = $req->new_response();
-        $res->redirect( $self->self_url . $record->id . '/view' );
+        $res->redirect( $self->after_POST( $record->id ) );
         return $res;
     }
     $form->field( 'submit' )->value( 'Create' );
@@ -129,7 +134,7 @@ sub edit {
     );
     if( $req->method eq 'POST' && $form->process() ){
         my $res = $req->new_response();
-        $res->redirect( $self->self_url . $record->id . '/view' );
+        $res->redirect( $self->after_POST( $record->id ) );
         return $res;
     }
     $form->field( 'submit' )->value( 'Update' );
@@ -175,4 +180,6 @@ This is experimental Template Tookit and DBIx::Class based CRUD controller for L
 =head2 view
 
 =head2 columns
+
+=head2 after_POST
 
